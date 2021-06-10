9663e4df7e0360a84a468854a856b01512e8ffee
plaetzaw <plaetzaw@gmail.com>
2021-06-09 19:11:24 -0500

    Initial login working

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import { useState, useContext } from 'react'
 import { UserContext } from './components/organisms/UserContext'
+import axios from 'axios'
 import Head from 'next/head'
 
 const Home = () => {
   const msg = useContext(UserContext)
   const [user, setUser] = useState(null)
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
 
   const Login = async () => {
     return {
@@ -12,6 +15,16 @@ const Home = () => {
       username: 'MysticPlato',
       email: 'admin@aol.com'
     }
+  }
+
+  const onClick = async (e) => {
+    e.preventDefault()
+    const LoginObj = {
+      email: email,
+      password: password
+    }
+    const request = await axios.post('http://localhost:8080/login', LoginObj)
+    console.log(request)
   }
 
   return (
@@ -34,6 +47,19 @@ const Home = () => {
         }}
         >swap
         </button>
+      </div>
+      <div>
+        <form>
+          <input
+            value={email}
+            onChange={(e) => { setEmail(e.target.value) }}
+          />
+          <input
+            value={password}
+            onChange={(e) => { setPassword(e.target.value) }}
+          />
+          <button onClick={onClick}>Submit</button>
+        </form>
       </div>
 
     </div>
