7a95d3312689f5997631c19db20e97d43a6e105e
plaetzaw <plaetzaw@gmail.com>
2021-06-28 14:07:47 -0500

    Messed up UC export

--- pages/components/organisms/UserContext.js
+++ pages/components/organisms/UserContext.js
@@ -1,11 +1,3 @@
 import { createContext } from 'react'
 
-const UserContext = createContext(null)
-// export const UserContext = createContext({
-//   id: undefined,
-//   username: undefined,
-//   email: undefined,
-//   token: undefined
-// })
-
-export default UserContext
+export const UserContext = createContext(null)
